1c94023a65c68706a2a9764149f386aed3789aaa
nakayama <nakayama@nakayamhinnoair>
2024-01-20 21:18:34 +0900

    バリデーションの追加

--- src/main/java/com/nakayama/form/BookForm.java
+++ src/main/java/com/nakayama/form/BookForm.java
@@ -1,19 +1,25 @@
 package com.nakayama.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-
 
 @Data
 public class BookForm {
-	
-	/**
-	 * 本のタイトルを保持
-	 */
-	private String title;
-		
-	/**
-	 * 本の値段を保持
-	 */
-	private Integer price;
 
+    /**
+     * 本のタイトルを保持
+     * チェック：空でないこと
+     */
+    @NotBlank(message = "タイトルを入力してください。")
+    private String title;
+
+    /**
+     * 本の値段を保持
+     * チェック：正の値であること
+     */
+    @NotNull(message = "値段を入力してください。")
+    @Positive(message = "値段は正の値にしてください。")
+    private Integer price;
 }
